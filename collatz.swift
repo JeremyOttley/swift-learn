dd094b008f9e3a15a75326999611094bd6453420
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-17 09:39:47 -0500

    Update collatz.swift

--- collatz.swift
+++ collatz.swift
@@ -1,3 +1,15 @@
+func greater_than_zero(n: Int) -> Bool {
+  return n > 0
+}
+
+func is_even(n: Int) -> Bool {
+  return n % 2 == 0
+}
+
+func is_odd(n: Int) -> Bool {
+  return n % 2 != 0
+}     
+
 func collatz(n: Int) -> Int {
   switch n {
     case greater_than_zero(n: n) && is_even(n: n):
