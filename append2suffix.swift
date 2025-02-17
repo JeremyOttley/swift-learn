1b75ab0ca71c1b9352aea99596233d1b73f49f34
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-17 09:51:09 -0500

    Create append2suffix.swift

---
+++ append2suffix.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+func append2suffix(f: String) -> String {
+
+  // Get filename without extension
+  let baseName = URL(fileURLWithPath: f)
+    .deletingPathExtension()
+    .lastPathComponent
+
+  // Get extension
+  let ext = URL(fileURLWithPath: f).pathExtension
+  
+  let newName = baseName + "2" + ext
+  return newName
+}
+
+print(append2suffix(f: "image.jpeg"))
