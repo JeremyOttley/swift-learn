96f45555e3e654634f919a1787db53b48c4a6f6b
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-18 12:22:42 -0500

    Update collatz.swift

--- collatz.swift
+++ collatz.swift
@@ -12,9 +12,9 @@ func isOdd(n: Int) -> Bool {
 
 func collatz(n: Int) -> Int {
   switch n {
-    case greater_than_zero(n: n) && is_even(n: n):
+    case greater_than_zero(n: n) && isEven(n: n):
         return n / 2
-    case greater_than_zero(n: n) && is_odd(n: n):
+    case greater_than_zero(n: n) && isOdd(n: n):
         return (n * 3) + 1
     default:
         return n
