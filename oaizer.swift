5aa193a9ee356e440e694e6af4af0ecc2b9fd419
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-19 07:59:28 -0500

    Update oaizer.swift

--- oaizer.swift
+++ oaizer.swift
@@ -6,8 +6,11 @@ let ppv = "PPV_BV"
 do {
   let isbn = "978-1-4780-2737-9"
   let hyphen = try Regex("-")
-  let product = pref + isbn.replacing(hyphen, with: "") + suffix //dropFirst(7), dropLast(), also this is a String.SubSequence and needs to be joined into a string
+  let cleanISBN = isbn.replacing(hyphen, with: "")
+  let stub = cleanISBN.dropFirst(7).dropLast()
+  let product = pref + stub + suffix 
   print(product)
 } catch {
   print("Product does not exist: \(error)")
 }
+
