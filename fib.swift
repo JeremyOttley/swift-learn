570fa09e80b0bc47b66b52afee87b1010d1ae593
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-26 12:54:07 -0500

    Update fib.swift

--- fib.swift
+++ fib.swift
@@ -1,8 +1,8 @@
 func fibonacci(_ n: Int) -> Int {
-  if n < 2 {
-    return n
-  } else {
-    return fibonacci(n - 1) + fibonacci( n - 2)
-  }
+ guard n > 1 else {
+  return n
+ }
+  return fibonacci(n - 1) + fibonacci( n - 2)
 }
+
 print(fibonacci(12))
