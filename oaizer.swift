f297f812f29cf82be2658550ac1200a5c95fe2a1
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-27 07:54:21 -0500

    Update oaizer.swift

--- oaizer.swift
+++ oaizer.swift
@@ -1,16 +1,15 @@
-let pref = "A" //prefix is a keyword
-let suffix = "E"
-let ppv = "PPV_BV"
+let pref: String = "A" //prefix is a keyword
+let suffix: String = "E"
+let ppv: String = "PPV_BV"
 
 
 do {
-  let isbn = "978-1-4780-2737-9"
-  let hyphen = try Regex("-")
-  let cleanISBN = isbn.replacing(hyphen, with: "")
-  let stub = cleanISBN.dropFirst(7).dropLast()
-  let product = pref + stub + suffix 
+  let isbn: String = "978-1-4780-2737-9"
+  let hyphen: Regex<AnyRegexOutput> = try Regex("-")
+  let cleanISBN: String = isbn.replacing(hyphen, with: "")
+  let stub: Substring = cleanISBN.dropFirst(7).dropLast()
+  let product: String = pref + stub + suffix 
   print(product)
 } catch {
   print("Product does not exist: \(error)")
 }
-
