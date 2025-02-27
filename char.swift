d7218266f2066631f3a84d5fd4217ef1fd487807
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-27 13:35:50 -0500

    Create char.swift

---
+++ char.swift
@@ -0,0 +1,11 @@
+// Creating a character
+var letter: Character = "H"    // Must use Character keyword
+var symbol: Character = "@"    // Can be any single symbol
+var space: Character = " "     // Even spaces are characters
+
+// Important: Single quotes don't work in Swift
+// let wrong: Character = 'H'  // This will cause a compiler error
+
+// Characters vs Strings
+let singleChar: Character = "A"    // Single character
+let multipleChars: String = "ABC"  // String (multiple characters)
