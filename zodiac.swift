4ed0dccc7a5c9bf54c5d99e492d75d39ec5c534a
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-28 09:43:24 -0500

    Update zodiac.swift

--- zodiac.swift
+++ zodiac.swift
@@ -1,4 +1,6 @@
 import Foundation 
+import FoundationNetworking
+//import FoundationXML
 
 typealias Fortune = String
 
@@ -19,6 +21,24 @@ enum Sign: String {
 
 typealias Horoscope = (sign: Sign, fortune: Fortune)
 
-let horoscope: Horoscope = (sign: .Leo, fortune: "A new opportunity will arise today.")
-print(horoscope.sign) 
-print(horoscope.fortune)
+func genHoroscope(_ sign: Sign ) async {
+  let fetchTask = Task {
+    let url = URL(string: "https://www.astrology.com/horoscope/daily/\(sign).html")!
+    let (data, _) = try await URLSession.shared.data(from: url)
+    return String(decoding: data, as: UTF8.self)
+  }
+let result = await fetchTask.result
+do {
+  let resultString = try result.get()
+  print(resultString)
+} catch {
+    print("Unknown error.")
+  }
+}
+
+await genHoroscope(.Libra)
+
+//let horoscope: Horoscope = (sign: .Leo, fortune: "A new opportunity will arise today.")
+//print(horoscope.sign) 
+//print(horoscope.fortune)
+//https://www.astrology.com/horoscope/daily/\(sign).html
