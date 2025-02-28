7fb2a17842cc733976c4b07863af59f250cf23f5
Jeremy Ottley <Gazaunga@gmail.com>
2025-02-28 14:13:24 -0500

    Create swisbn.swift

---
+++ swisbn.swift
@@ -0,0 +1,33 @@
+import Foundation 
+
+enum IsbnError {
+  case InvalidIsbn
+  case InvalidCheckdigit
+  case InvalidLength
+  case RegistrantNotFound
+}
+
+func isValid(_ isbn: String) -> Bool {
+  //turn into Guard?
+  isLengthCorrect(isbn) && isCharsCorrect(isbn) && isCheckDigitValid(isbn)
+}
+
+func isCheckDigitValid(_ isbn: String) -> Bool {
+  let normalizedIsbn = normalize(isbn)
+  let len = normalizedIsbn.count
+  guard let len > 8 || len < 13 else {
+    return false
+  }
+  switch isbn {
+    case len == 10:
+      isbn10CheckDigit(normalizedIsbn)
+    default:
+      isbn13CheckDigit(normalizedIsbn)
+  }
+
+  // if let lastDigit = normalizedIsbn.last {
+  //   digit == lastDigit
+  // }
+}
+
+https://github.com/solar05/glisbn/blob/main/src/glisbn.gleam
