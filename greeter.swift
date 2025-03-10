05576acd9b7dca05f50c94970fde03bdaccfd41e
Jeremy Ottley <Gazaunga@gmail.com>
2025-03-10 12:12:05 -0400

    Update greeter.swift

--- greeter.swift
+++ greeter.swift
@@ -2,7 +2,17 @@ print("Please enter your name: ")
 
 let name = readLine()
 
-func greeter(name: String) -> () {
+func greeter(_ name: String) -> () {
+  
+  guard !name.isEmpty else {
+    print("No name entered!")
+    return
+  }
+  guard !name.allSatisfy({ $0.isWhitespace }) else {
+          print("Name cannot be empty or contain only whitespace")
+          return
+      }
+
   print("Hello \(name)!")
 }
 
