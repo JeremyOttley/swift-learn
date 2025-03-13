68ae41771c2f9067ad441d2f4b7cd5a7a4c436a0
Jeremy Ottley <Gazaunga@gmail.com>
2025-03-13 11:33:06 -0400

    Update byteString.swift

--- byteString.swift
+++ byteString.swift
@@ -1,25 +1,15 @@
-// Simple
-
-import Foundation
-
-// Convert String to bytes
-let string = "Hello"
-let bytes = Array(string.utf8)
-
-// Convert bytes back to String
-if let result = String(bytes: bytes, encoding: .utf8) {
-    print(result) // Prints: Hello
+func stringToBytes(s: String) -> Array<UInt8> {
+  return Array(s.data(using: .utf8)!) 
 }
 
-
-// Idiomatic Swift (but more memory usage, both require foundation):
-
-// Convert String to Data
-let string = "Hello"
-let data = string.data(using: .utf8)!
-let bytes = Array(data)
-
-// Convert back to String
-if let result = String(data: Data(bytes), encoding: .utf8) {
+func bytesToString(b: Array<UInt8>) {
+  if let result = String(data: Data(b), encoding: .utf8) {
     print(result) // Prints: Hello
+  }
 }
+
+let x = "Jeremy"
+
+let y = stringToBytes(s: x)
+print(y)
+bytesToString(b: y)
