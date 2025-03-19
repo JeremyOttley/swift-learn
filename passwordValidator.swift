6fe17e7564ffbc31b5694a5782eb567f3c837691
Jeremy Ottley <Gazaunga@gmail.com>
2025-03-19 10:29:02 -0400

    Update passwordValidator.swift

--- passwordValidator.swift
+++ passwordValidator.swift
@@ -1,41 +1,53 @@
-//https://i18n-puzzles.com/puzzle/3/
-
 import Foundation
 
-// Create common character sets
-let letters = CharacterSet.letters //letters.contains("e")
-let digits = CharacterSet.decimalDigits
-
+//https://i18n-puzzles.com/puzzle/3/
 
 func checkASCII(_ string: String) -> Bool {
     return string.utf8.reduce(true) { $0 && ($1 <= 127) }
 }
 
-// let x = checkASCII("Hello123")  // Prints nothing (all ASCII)
-// let y = checkASCII("Hello🌍")   // Prints "error" (contains emoji)
-
-// print(x)
-// print(y)
-
-//password requirements
-
-// a length of at least 4 and at most 12
-guard s.count >= 4 && s.count <= 12 else {
-  print("Character count must be between 4 and 12")
-  return false
-}
-// at least one digit
-guard s.count >= 1 else { //digits.contains()
-  print("Must have at leasat one digit")
-  return false
-}
-// at least one uppercase letter (with or without accents, examples: A or Ż)
-guard // if uppers.contains does not have any member of s
-// at least one lowercase letter (with or without accents, examples: a or ŷ)
-guard // if lowers.contains does not have any member of s
-// at least one character that is outside the standard 7-bit ASCII character set (examples: Ű, ù or ř)
-guard checkASCII(s) else {
-  print("Not all characters are ASCII")
-  return false
+func hasNonASCII(_ s: String) -> Bool {
+  return s.unicodeScalars.contains { $0.value > 127 }
 }
 
+func hasLetters(in s: String) -> Bool {
+  return s.rangeOfCharacter(from: .letters) != nil
+}
+
+func hasDigits(in s: String) -> Bool {
+  return s.rangeOfCharacter(from: .decimalDigits) != nil
+}
+
+func hasUppers(in s: String) -> Bool {
+  return s.rangeOfCharacter(from: .uppercaseLetters) != nil
+}
+
+func isPassValid(_ p: String) -> Bool {
+  guard p.count >= 4 && p.count <= 12 else {
+    print("Character count must be between 4 and 12")
+    return false
+  }
+
+  guard hasDigits(in: p) else { 
+    print("Must have at least one digit")
+    return false
+  }
+
+  guard hasUppers(in: p) else {
+    print("Must have at least one uppercased letter")
+    return false
+  }
+  guard hasLetters(in: p) else {
+    print("Must have at least one letter")
+    return false
+  }
+
+  guard hasNonASCII(p) else {
+    print("All characters are 7-bit ASCII")
+    return false
+  }
+  return true
+}
+
+let z = isPassValid("nut9ù8nvfjD")
+print(z)
