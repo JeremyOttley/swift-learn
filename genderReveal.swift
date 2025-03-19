a7f9c1b079681d39fe6862ee83db51d0a27dc269
Jeremy Ottley <Gazaunga@gmail.com>
2025-03-19 13:26:56 -0400

    Update genderReveal.swift

--- genderReveal.swift
+++ genderReveal.swift
@@ -1,1 +1,15 @@
+import Foundation
+
 let url = "https://api.genderize.io?name="
+
+//{"count":337362,"name":"jeremy","gender":"male","probability":1.0}
+struct Response {
+  var count: Int
+  var name: String
+  var gender: String
+  var probability: Double
+}
+
+let res = Response(count: 33762, name: "jeremy", gender: "male", probability: 1.0)
+
+print(res.gender)
