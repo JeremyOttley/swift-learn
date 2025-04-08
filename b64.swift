debf3dac1b6e8770bd056f4b84684dcde2ce894c
Jeremy Ottley <Gazaunga@gmail.com>
2025-04-08 11:17:49 -0400

    Update b64.swift

--- b64.swift
+++ b64.swift
@@ -2,19 +2,20 @@ import Foundation
 
 let input = "Jeremy Mark Ottley"
 
-func bEncode(_ s: String) -> String {
-  return s.data(using: .utf8)!.base64EncodedString()
+func bEncode(_ s: String) -> String? {
+  return s.data(using: .utf8)?.base64EncodedString()
 }
 
-func bDecode(_ s: String) -> String {
+func bDecode(_ s: String) -> String? {
   
   guard let data = Data(base64Encoded: s) else { 
-    return "" 
+    return nil
   }
 
-  return String(data: data, encoding: .utf8)!
+  return String(data: data, encoding: .utf8)
 
 }
 
-let x = bDecode(bEncode(input))
-print(x)
+if let x = bEncode(input) {
+  print(bDecode(x) ?? "") // ?? is like unwrap_or_default
+}
