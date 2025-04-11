4f07071e2f2ae0e279add77941c1a0bfcc5b2d00
Jeremy Ottley <Gazaunga@gmail.com>
2025-04-11 12:51:11 -0400

    Update result.swift

--- result.swift
+++ result.swift
@@ -1,19 +1,26 @@
-enum UserError: Error {
-    case invalidEmail
+enum ISBNError: Error {
+    case invalidISBN
+    case noHyphen
 }
 
-func validateEmail(_ email: String) -> Result<String, UserError> {
-    guard email.contains("@") else {
-        return .failure(.invalidEmail)
+let x = "978-1-4780-1029-6"
+let y = "9781478010296"
+
+func validateISBN(_ isbn: String) -> Result<String, ISBNError> {
+    guard isbn.contains("-") else {
+        return .failure(.noHyphen)
     }
-    return .success(email)
+    guard isbn.count >= 16 else {
+    return .failure(.invalidISBN)
+    }
+    return .success(isbn)
 }
 
 // Usage
-let result = validateEmail("test@example.com")
+let result = validateISBN(x)
 switch result {
-case .success(let email):
-    print("Valid email: \(email)")
+case .success(let isbn):
+    print("Valid ISBN: \(isbn)")
 case .failure(let error):
     print("Error: \(error)")
 }
