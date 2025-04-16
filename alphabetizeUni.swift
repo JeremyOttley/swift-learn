9b916b0b752fc9a522b9e832edf4ad13072508c8
Jeremy Ottley <Gazaunga@gmail.com>
2025-04-16 09:15:30 -0400

    Update alphabetizeUni.swift

--- alphabetizeUni.swift
+++ alphabetizeUni.swift
@@ -4,12 +4,15 @@ import Foundation
 
 let input = ["Ñíguez Peña, María de los Ángeles: 0151605", "Åberg, Rosa-Maria: 0110966", "Özaydın, Zeynep: 0185292", "van den Heyden, Harm: 0168131", "Ämtler, Lorena: 0112717", "Olofsson, Mikael: 0103652", "van Leeuwen, Joke: 0172199", "Vandersteen, Willy: 0120659", "Østergård, Magnus: 0113959", "van Leeuw, Floor: 0144158", "Navarrete Ortiz, Dolores: 0119411", "Aalto, Alvar: 0192872", "Zondervan, Jan Peter: 0103008", "Æbelø, Aurora: 0113267", "O'Neill, Cara: 0109551"]
 
-let separators = CharacterSet(charactersIn: " ,;")
+let separators = CharacterSet(charactersIn: " ,:")
 
 func flattenStr(_ s: String) -> Array<String> {
   return s.components(separatedBy: separators)
           .filter { !$0.isEmpty }
 }
 
-let x = flattenStr(input[0])
-print(x.sorted { $0.localizedStandardCompare($1) == .orderedAscending })
+func alphabetize(text: String) -> Array<String> {
+  return flattenStr(text).sorted { $0.localizedStandardCompare($1) == .orderedAscending }
+}
+
+print(alphabetize(text: input[0]))
